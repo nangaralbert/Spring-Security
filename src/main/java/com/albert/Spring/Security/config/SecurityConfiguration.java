c7e29ee5d3631e7e6821b4d95c05b810a2c41893
Albert Nangara <nangaralbert@gmail.com>
2025-04-11 15:12:14 +0200

    Basic Auth with In-Memory user details and Authorization  globally
    Basic Auth with In-Memory user details specified in the config file and Authorization globally of certain endpoints for example the public is authorized to access the /api/v1/demo/home endpoints while other required authentication.

--- src/main/java/com/albert/Spring/Security/config/SecurityConfiguration.java
+++ src/main/java/com/albert/Spring/Security/config/SecurityConfiguration.java
@@ -2,8 +2,10 @@ package com.albert.Spring.Security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -31,5 +33,15 @@ public class SecurityConfiguration {
         return new InMemoryUserDetailsManager(admin,userDetailsOne,userDetailsTwo);
     }
 
- 
+    @Bean
+    public  SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf(csrfCustomizer->csrfCustomizer.disable());
+        httpSecurity.authorizeHttpRequests(request ->
+                request.requestMatchers("/api/v1/demo/home").permitAll()
+                        .anyRequest().authenticated());
+        httpSecurity.httpBasic(Customizer.withDefaults());
+        httpSecurity.sessionManagement(session->
+                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        return httpSecurity.build();
+    }
 }
--- src/main/java/com/albert/Spring/Security/controller/DemoController.java
+++ src/main/java/com/albert/Spring/Security/controller/DemoController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/demo")
 public class DemoController {
 
-    @GetMapping("/")
+    @GetMapping("/home")
     public String home(){
         return "This is a public API";
     }
